5764496b8a7218decdc63f01e637ff19ca886c66
huangzhongkai <843920377@qq.com>
2018-04-29 16:31:15 +0800

    init

---
+++ caches/error_log.php
@@ -0,0 +1,3 @@
+<?php exit;?>04-29 14:44:36 | 2 | mysqli_connect(): (HY000/2002): No such file or directory | /Users/huangzhongkai/MyPhpSite/phpcms/install/install.php | 359
+<?php exit;?>04-29 15:11:07 | 2048 | Only variables should be passed by reference | phpcms/modules/member/index.php | 227
+<?php exit;?>04-29 15:11:54 | 2048 | Only variables should be passed by reference | phpcms/modules/member/index.php | 227
---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://localhost/phpcms/',
+    'authkey' => '1uec7zdmytl9iU0EKX42FEP6nm4MUT8G',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
